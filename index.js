db2fbd9f5ab63846bfaea645e074f8bfb6b78915
eliasCR21 <45535878+eliasCR21@users.noreply.github.com>
2019-05-25 18:00:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
-const prefix = ("?");
+const prefix = (cfg.prefix);
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
 });
 
-bot.on('guildMemberAdd', member => {
-    member.createDM().then(channel => {
-        return channel.send('Wesh' + member.displayName)
-        console.log(`${member.displayName} à rejoint le serveur.`)
-    }).catch(console.error)
-});
 
-const ban = require('./kick et ban/ban');
-require('./embed/RichEmbed');
-
-bot.on('message', function (message){
-    if (ban.match(message)){
-        return ban.action(message)
-    }
-});
-
-
-bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-
-});
 
 bot.login(token);
